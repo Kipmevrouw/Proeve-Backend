d91c9a82e665d3ab92f078eeae55dcb76b905785
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2024-05-27 20:51:06 +0200

    update server.js

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ const db = mysql.createConnection({
   host: MYSQL_ADDON_HOST,
   database: MYSQL_ADDON_DB,
   user: MYSQL_ADDON_USER,
-  password: MYSQL_ADDON_PASSWORD,
-  port: MYSQL_ADDON_PORT
+  password: MYSQL_ADDON_PASSWORD
 });
 
 app.post("/signup", (req, res) => {
@@ -95,7 +94,7 @@ db.query('SELECT 1 + 1 AS result', (error, results, fields) => {
   console.log('Database connection successful:', results[0].result);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = MYSQL_ADDON_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
