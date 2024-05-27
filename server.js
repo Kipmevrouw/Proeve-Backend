60ab46fde3ea9295b59d0f47011fd96cd8208480
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2024-05-27 20:45:10 +0200

    update server.js

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: process.env.MYSQL_ADDON_HOST,
-  database: process.env.MYSQL_ADDON_DB,
-  user: process.env.MYSQL_ADDON_USER,
-  password: process.env.MYSQL_ADDON_PASSWORD
+  host: MYSQL_ADDON_HOST,
+  database: MYSQL_ADDON_DB,
+  user: MYSQL_ADDON_USER,
+  password: MYSQL_ADDON_PASSWORD
 });
 
 app.post("/signup", (req, res) => {
