973d5b0c6e44d0a928be9373069ad7c59b53b49e
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2024-05-27 19:49:12 +0200

    update server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.post("/login", (req, res) => {
   );
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server draait op poort ${PORT}`);
 });
