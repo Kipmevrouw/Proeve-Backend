bd643845b4d2648a57365675f55c9441919359da
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2024-05-28 18:04:25 +0200

    update server.js

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const saltRounds = parseInt(process.env.SALT_ROUNDS, 10);
+
 const app = express();
 const corsOptions = {
   origin: "https://techyourtalentamsterdam.nl"
 };
 
 app.use(cors(corsOptions));
+
 app.use(express.json());
 
-// MySQL connection pool
-const pool = mysql.createPool({
-  connectionLimit: 10,
+const db = mysql.createConnection({
   host: process.env["MYSQL_ADDON_HOST"],
   database: process.env["MYSQL_ADDON_DB"],
   user: process.env["MYSQL_ADDON_USER"],
@@ -25,17 +25,11 @@ const pool = mysql.createPool({
   port: process.env["MYSQL_ADDON_PORT"],
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Internal server error" });
-});
-
 app.post("/signup", (req, res) => {
   console.log("Ontvangen gegevens:", req.body);
-  const sql = "INSERT INTO gebruiker (`voornaam`, `achternaam`, `school`, `code`, `uitslag1`, `uitslag2`, `wachtwoord`, `akkoort_voorwaarden`) VALUES (?)";
+  const sql =
+    "INSERT INTO gebruiker (`voornaam`, `achternaam`, `school`, `code`, `uitslag1`, `uitslag2`, `wachtwoord`, `akkoort_voorwaarden`) VALUES (?)";
   const wachtwoord = req.body.wachtwoord;
-
   bcrypt.hash(wachtwoord.toString(), saltRounds, (err, hash) => {
     console.log("bcrypt done");
     if (err) {
@@ -52,45 +46,46 @@ app.post("/signup", (req, res) => {
       hash,
       req.body.akkoort_voorwaarden,
     ];
-    console.log("values gelezen");
-
-    pool.query(sql, [values], (err, data) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({ error: "Internal server error" });
-      }
-      console.log("toegevoegd aan database");
+    console.log("values gelezen")
+    db.query(sql, [values], (err, data) => {
+      if (err) return res.json(err);
       return res.json(data);
     });
+    console.log("toegevoegd aan database")
   });
 });
 
 app.post("/login", (req, res) => {
-  const sql = "SELECT * FROM gebruiker WHERE voornaam = ? AND achternaam = ? AND code = ?";
-  pool.query(sql, [req.body.voornaam, req.body.achternaam, req.body.code], (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: "Internal server error" });
-    }
-
-    if (data.length === 0) {
-      return res.status(401).json({ error: "User not found" });
-    }
-
-    const hashedPassword = data[0].wachtwoord;
-
-    bcrypt.compare(req.body.wachtwoord, hashedPassword, (err, result) => {
+  const sql =
+    "SELECT * FROM gebruiker WHERE voornaam = ? AND achternaam = ? AND code = ?";
+  db.query(
+    sql,
+    [req.body.voornaam, req.body.achternaam, req.body.code],
+    (err, data) => {
       if (err) {
         console.error(err);
         return res.status(500).json({ error: "Internal server error" });
       }
-      if (result) {
-        return res.json({ success: "Login successful" });
-      } else {
-        return res.status(401).json({ error: "Incorrect credentials" });
+
+      if (data.length === 0) {
+        return res.status(401).json({ error: "User not found" });
       }
-    });
-  });
+
+      const hashedPassword = data[0].wachtwoord;
+
+      bcrypt.compare(req.body.wachtwoord, hashedPassword, (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Internal server error" });
+        }
+        if (result) {
+          return res.json({ success: "Login successful" });
+        } else {
+          return res.status(401).json({ error: "Incorrect credentials" });
+        }
+      });
+    }
+  );
 });
 
 // db.query('SELECT 1 + 1 AS result', (error, results, fields) => {
